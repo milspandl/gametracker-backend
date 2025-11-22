d0c90848edc68a93f6d82920cbac95600189e796
milspandls <milagrospandulli@gmail.com>
2025-11-22 11:12:55 -0300

    Asegurar puerto para Render

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 connectDB();
-
 
 app.use("/api/games", require("./routes/gameRoutes"));
 app.use("/api/reviews", require("./routes/reviewRoutes"));
 
 
-app.listen(process.env.PORT, () =>
-console.log(`Servidor funcionando en puerto ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en puerto ${PORT}`);
+});
